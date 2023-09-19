3b73857e05fbf486420c456228e8a0d01b9437c9
Manoj-Gunaseelaraj <138764668+Manoj-Gunaseelaraj@users.noreply.github.com>
2023-09-19 10:54:06 +0530

    Add files via upload

---
+++ Travel-vlog/script.js
@@ -0,0 +1,7 @@
+const nav_button= document.querySelector(".btn-mobile-nav");
+const nav = document.querySelector(".nav-lists");
+
+nav_button.addEventListener("click",function(e){
+   
+    nav_button.classList.add(nav)
+})
